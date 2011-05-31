6e631d0ee77679f9a6bafcb362bcdf9ed6a2ec3a
Brandon R. Stoner <monokrome@limpidetch.com>
2011-05-31 14:40:54 -0600

    added static files.

---
+++ demo/static/lib/main.js
@@ -0,0 +1,27 @@
+// TODO: Test whether or not require.js is aware of this module
+//       having already been loaded upon executing a require('main')
+//       from other modules. If not, this will break the "bootstrapped"
+//       mechanism.
+
+(function main () {
+	/**
+	 * Represents the main entry-point of our application. Provides
+	 * require.js the necessary configuration that we required in order
+	 * to bootstrap the codebase.
+	 */
+	var dependancies = ['mediaSlider'],
+
+	// Some other modules may want to wait for this value to be true.
+	bootstrapped = false,
+
+	options = {
+		baseUrl: '/lib/',
+		ready: (function load_dependancies () {
+			require(dependancies, function bootstrap_complete () {
+				bootstrapped = true;
+			});
+		})
+	};
+
+	require(options);
+})();
